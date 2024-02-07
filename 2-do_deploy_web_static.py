3410ea78542bdc39ec4d55651d91342ed860ccd7
Pronothurah <onsongojosphat28@gmail.com>
2024-02-07 15:24:17 +0300

    fix: deploying webstatic

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -10,57 +10,52 @@ env.user = "ubuntu"
 env.hosts = ["52.86.50.144", "52.3.250.50"]
 
 
+@runs_once
 def do_pack():
-    """Compress files from web_static directory"""
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
     try:
-        if not os.path.isdir("versions"):
-            os.makedirs("versions")
-        date = datetime.now()
-        file = "versions/web_static_{0}{1}{2}{3}{4}{5}".format(
-            date.year,
-            date.month,
-            date.day,
-            date.hour,
-            date.minute,
-            date.second
-        )
-        file += ".tgz"
-        local("tar -cvzf {} web_static".format(file))
-        return file
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
     except Exception:
-        return None
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
-    """
-    Deploy archive
-
+    """Deploys the static files to the host servers.
     Args:
-        - archive_path(str, optional): Path of the archive
+        archive_path (str): The path to the archived static files.
     """
-    try:
-        if not os.path.isfile(archive_path):
-            return False
-        path = archive_path.split("/")[1]
-        name = path.split(".")[0]
-        put(archive_path, "/tmp/{0}".format(path))
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(name))
-        source = "sudo tar -xzf /tmp/{0} -C".format(path)
-        dest = "/data/web_static/releases/{0}/".format(name)
-        run(source + " " + dest)
-        run("sudo rm /tmp/{0}".format(path))
-        source = (
-            "sudo mv -fT /data/web_static/releases/{0}/web_static/.* .".format(name)
-        )
-        dest = "/data/web_static/releases/{0}/".format(name)
-        run(source + " " + dest)
-        run(
-            "sudo rm -rf /data/web_static/releases/{0}/web_static".format(name)
-        )
-        run("sudo rm -rf /data/web_static/current")
-        source = "sudo ln -s /data/web_static/releases/{0}/".format(name)
-        dest = "/data/web_static/current"
-        run(source + " " + dest)
-        return True
-    except Exception:
+    if not os.path.exists(archive_path):
         return False
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
+    try:
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
