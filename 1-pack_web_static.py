26b3c9c69606af6e1007663821a6bfb5a4c5714d
Pronothurah <onsongojosphat28@gmail.com>
2024-02-07 14:24:52 +0300

    packing task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,7 +4,7 @@ Generates a .tgz archive from the contents of
 the web_static folder of your AirBnB Clone repo,
 using the function do_pack
 """
-import os
+from os.path import getsize
 from datetime import datetime
 from fabric.api import *
 
@@ -16,6 +16,9 @@ def do_pack():
         date = datetime.now()
         t_string = date.strftime('%Y%m%d%H%M%S')
         local(f'sudo tar -cvzf versions/web_static_{t_string}.tgz web_static')
+        f_path = f"versions/web_static_{t_string}.tgz"
+        f_size = getsize(f_path)
+        print(f_size)
         # return file
     except FileNotFoundError:
         print("Error: Unable to find specified directory.")
