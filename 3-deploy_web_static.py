accfbe6bde3d4708642352f4164abc85bd2ea896
Pronothurah <onsongojosphat28@gmail.com>
2024-02-07 16:36:11 +0300

    fix: 3-deploy

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Distributes an archive to your web servers,
-using the function do_deploy
+Distributes an archive to your web servers, using the function do_deploy
 """
 import os
 from datetime import datetime
@@ -11,59 +10,66 @@ env.user = "ubuntu"
 env.hosts = ["52.86.50.144", "52.3.250.50"]
 
 
+@runs_once
 def do_pack():
-    """Compress files from web_static directory"""
+    """Archives the static files."""
+    if not os.path.isdir("versions"):
+        os.mkdir("versions")
+    cur_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+        cur_time.year,
+        cur_time.month,
+        cur_time.day,
+        cur_time.hour,
+        cur_time.minute,
+        cur_time.second
+    )
     try:
-        if not os.path.isdir("versions"):
-            os.makedirs("versions")
-        date = datetime.now()
-        file = "versions/web_static_{0}{1}{2}{3}{4}{5}".format(
-            date.year,
-            date.month,
-            date.day,
-            date.hour,
-            date.minute,
-            date.second
-        )
-        file += ".tgz"
-        local("tar -cvzf {} web_static".format(file))
-        return file
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        archize_size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, archize_size))
     except Exception:
-        return None
+        output = None
+    return output
 
 
 def do_deploy(archive_path):
-    """
-    Deploy archive
-
+    """Deploys the static files to the host servers.
     Args:
-        - archive_path(str, optional): Path of the archive
+        archive_path (str): The path to the archived static files.
     """
+    if not os.path.exists(archive_path):
+        return False
+
     try:
-        if not os.path.isfile(archive_path):
-            return False
-        path = archive_path.split("/")[1]
-        name = path.split(".")[0]
-        put(archive_path, "/tmp/{0}".format(path))
-        run("sudo mkdir -p /data/web_static/releases/{}/".format(name))
-        source = "sudo tar -xzf /tmp/{0} -C".format(path)
-        dest = "/data/web_static/releases/{0}/".format(name)
-        run(source + " " + dest)
-        run("sudo rm /tmp/{0}".format(path))
-        source = (
-            "sudo mv /data/web_static/releases/{0}/web_static/*".format(name)
-        )
-        dest = "/data/web_static/releases/{0}/".format(name)
-        run(source + " " + dest)
-        run(
-            "sudo rm -rf /data/web_static/releases/{0}/web_static".format(name)
-        )
-        run("sudo rm -rf /data/web_static/current")
-        source = "sudo ln -s /data/web_static/releases/{0}/".format(name)
-        dest = "/data/web_static/current"
-        run(source + " " + dest)
+        # Upload archive to /tmp directory on remote server
+        put(archive_path, "/tmp")
+
+        folder_name = os.path.basename(archive_path).split('.')[0]
+        folder_path = f"/data/web_static/releases/{folder_name}"
+        run(f"mkdir -p {folder_path}")
+        run(f"tar -xzf /tmp/{os.path.basename(archive_path)} -C {folder_path}")
+
+        # Remove archive from /tmp directory
+        run(f"rm /tmp/{os.path.basename(archive_path)}")
+
+        # Move contents of extracted folder to current release directory
+        run(f"mv {folder_path}/web_static/* {folder_path}/")
+
+        # Remove empty web_static directory
+        run(f"rm -rf {folder_path}/web_static")
+
+        # Remove current symbolic link
+        run("rm -rf /data/web_static/current")
+
+        # Create new symbolic link
+        run(f"ln -s {folder_path} /data/web_static/current")
+
+        print('New version deployed!')
         return True
-    except Exception:
+    except Exception as e:
+        print(f"Failed to deploy: {e}")
         return False
 
 
@@ -71,7 +77,7 @@ def deploy():
     """
     Full deployment based on do_pack & do_deploy (task 2)
     """
-    path = do_pack()
-    if path is None:
+    archive_path = do_pack()
+    if archive_path is None:
         return False
-    return do_deploy(path)
+    return do_deploy(archive_path)
