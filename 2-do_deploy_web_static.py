bc1834fb0b0dd8cc61da5ab6e02e40f240e8d0c2
Pronothurah <onsongojosphat28@gmail.com>
2024-02-07 17:17:04 +0300

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -41,44 +41,21 @@ def do_deploy(archive_path):
     """
     if not os.path.exists(archive_path):
         return False
-
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
+    success = False
     try:
-        # Upload archive to /tmp directory on remote server
-        put(archive_path, "/tmp")
-
-        folder_name = os.path.basename(archive_path).split('.')[0]
-        folder_path = f"/data/web_static/releases/{folder_name}"
-        run(f"mkdir -p {folder_path}")
-        run(f"tar -xzf /tmp/{os.path.basename(archive_path)} -C {folder_path}")
-
-        # Remove archive from /tmp directory
-        run(f"rm /tmp/{os.path.basename(archive_path)}")
-
-        # Move contents of extracted folder to current release directory
-        run(f"mv {folder_path}/web_static/* {folder_path}/")
-
-        # Remove empty web_static directory
-        run(f"rm -rf {folder_path}/web_static")
-
-        # Remove current symbolic link
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
         run("rm -rf /data/web_static/current")
-
-        # Create new symbolic link
-        run(f"ln -s {folder_path} /data/web_static/current")
-
-        current_index_html = "/data/web_static/current/0-index.html"
-        current_my_index_html = "/data/web_static/current/my_index.html"
-
-        index_exists_cmd = f"test -f {current_index_html} && echo 'Exists'"
-        my_ix_exists_cmd = f"test -f {current_my_index_html} && echo 'Exists'"
-
-        if run(index_exists_cmd).stdout.strip() == "Exists" and \
-           run(my_ix_exists_cmd).stdout.strip() == "Exists":
-            print('New version deployed!')
-            return True
-        else:
-            return False
-
-    except Exception as e:
-        print(f"Failed to deploy: {e}")
-        return False
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
+        success = True
+    except Exception:
+        success = False
+    return success
