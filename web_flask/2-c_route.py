3530763c3e2d4806a00a311cfc31224a856b4a94
Pronothurah <onsongojosphat28@gmail.com>
2024-02-18 22:24:49 +0300

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''A simple Flask web application.
+'''
+
+from flask import Flask
+
+app = Flask(__name__)
+"""Flask Apllication instance"""
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def ctext(text):
+    return f'C {text.replace("_", " ")}'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
