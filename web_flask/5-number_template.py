a75953375a643a5087493ba9b4a407ba53db6f26
Pronothurah <onsongojosphat28@gmail.com>
2024-02-18 23:34:04 +0300

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,7 +43,7 @@ def number(n):
 @app.route('/number_template/<int:n>')
 def number_template(n):
     """Display content from html files for the route"""
-    return render_template('5-number.html', n=n)
+    return render_template('5-number.html', number = n)
 
 
 if __name__ == '__main__':
