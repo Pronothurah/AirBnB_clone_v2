400a192fcdb9215ddbe2a54c8ba6d3565f9d43ae
Pronothurah <onsongojosphat28@gmail.com>
2024-02-19 00:13:55 +0300

    7. Improve engines

--- models/state.py
+++ models/state.py
@@ -3,10 +3,9 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.city import City
+# from models.city import City
 from os import getenv
 import models
-import shlex
 
 
 class State(BaseModel, Base):
@@ -14,21 +13,12 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade="all, delete", backref="state")
-
+    
     if getenv("HBNB_TYPE_STORAGE", None) != "db":
         @property
         def cities(self):
             """The cities property"""
-            all_storage = models.storage.all()
-            elements = []
-            result = []
-            for key in all_storage:
-                city = key.replace(".", " ")
-                city = shlex.split(city)
-                if city[0] == "City":
-                    elements.append(all_storage[key])
-            for e in elements:
-                if e.state_id == self.id:
-                    result.append(e)
-            return result
+            all_storage = models.storage.all(models.City)
+            return [city for city in all_storage.values() if city.state_id == self.id]
+    else:
+        cities = relationship("City", cascade="all, delete", backref="state")
